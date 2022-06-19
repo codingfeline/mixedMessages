3804d75ee55ace5a97d5fa0b9d91171f4800a294
Nazie Sharid <nazie.sh4rid@gmail.com>
2022-06-19 18:22:22 +0100

    added meals array

---
+++ index.js
@@ -0,0 +1,9 @@
+const meals = [
+    'Mediterranean Salad',
+    'Double Quarter Pounder',
+    'Lamb Steak',
+    'KFC Bargain Bucket',
+    'Shish Kebab',
+    'Dominos',
+    'Rainbow Salad',
+  ]
